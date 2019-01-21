2646494f52ae1adeff51ef2ac20d40267887fa04
denisijcu <denisijcu266@gmail.com>
2019-01-21 16:42:48 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   
+ <header class="header">
+    <h1>TodoList</h1>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+  </header>
+
+  
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+
+ .header {
+    background: #333;
+    color: #fff;
+    text-align: center;
+    padding: 10px;
+  }
+
+  .header a {
+    color: #fff;
+    padding-right: 5px;
+    text-decoration: none;
+  }
 </style>
---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About ToDo</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio nesciunt id eum fugit ea perspiciatis eaque sunt consequuntur ipsam. Quibusdam commodi rem unde, reiciendis ullam veritatis delectus temporibus atque vel ipsa, sint nihil fugiat eos quia ea aliquam, ipsam provident!</p>
+  </div>
+</template>
